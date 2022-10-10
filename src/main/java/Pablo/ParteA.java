bdcb80e6932b0876acfebc68bb25422f040125bb
PabloDaw2022 <pnavgarb@g.educaand.es>
2022-10-10 14:40:28 +0200

    Commit

---
+++ src/main/java/Pablo/ParteA.java
@@ -0,0 +1,149 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pablo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pablo
+ */
+public class ParteA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        La idea es que tu programa ayude a los dueños de la tienda 
+        a saber cuánto han cobrado cada día y al final de la semana, 
+        suponiendo que todas las reparaciones cuestan 32.6€ (double)
+        y todas las semanas trabajan de lunes a viernes (5 días). 
+        Una vez leídos los datos del número de reparaciones que hay cada día 
+        (de lunes a viernes) se deben mostrar los datos de lo que se
+        ha cobrado cada día (float) y al final de la semana (float). Ten en cuenta que
+        se debe limitar la salida por consola a 3 números decimales.
+        En esta parte todos los datos deben leerse por teclado usando Scanner
+        y mostrarse usando la consola.
+        
+        
+         */
+
+        //Declaracion del scanner
+        Scanner teclado = new Scanner(System.in);
+        //variables constante
+        final double REPARACIONES = 32.6;
+        //variables
+        int numeroReparacionesDelDia;
+
+        float reparacionesTotalSemana;
+
+        float reparacionesLunes;
+        float reparacionesMartes;
+        float reparacionesMiercoles;
+        float reparacionesJueves;
+        float reparacionesViernes;
+
+        //Entrada de Datos
+        //Lunes
+        System.out.println(" Cuantas reparaciones a habido el lunes?");
+
+        //Poner el escaner que sea un Int.
+        numeroReparacionesDelDia = teclado.nextInt();
+        //Salida de datos de system out monstando el numero de 
+        //reparaciones diarias
+        System.out.println(" El numero de reparaciones de este lune son "
+                + numeroReparacionesDelDia);
+
+        //Formula de calculo del dia 
+        reparacionesLunes = (float) (REPARACIONES * numeroReparacionesDelDia);
+
+        //Resultado de las reparaciones del Lunes
+        //Que salgan 3 decimales despues de la coma
+        System.out.printf(" %.3f " + "€", reparacionesLunes);
+
+        //Martes
+        //Entrada de datos
+        System.out.printf(" Cuantas reparaciones a habido el Martes?");
+
+        //Poner el escaler que sea un Int
+        numeroReparacionesDelDia = teclado.nextInt();
+        //Salida de datos de system out
+        System.out.println(" El numero de reparaciones de este Martes son "
+                + numeroReparacionesDelDia);
+
+        //Formula de calculo del dia 
+        reparacionesMartes = (float) (REPARACIONES *numeroReparacionesDelDia);
+
+        //Resultado de las reparaciones del Martes
+        //salgan 3 decimales despues de la coma
+        System.out.printf(" %.3f " + "€", reparacionesMartes);
+
+        //Miercoles
+        //Entrada de datos
+        System.out.println(" Cuantas reparaciones a habido el Miercoles?");
+
+        //Poner el escamer que sea un Int
+        numeroReparacionesDelDia = teclado.nextInt();
+        //Salida de datos de system out
+        System.out.println(" El numero de reparaciones de este Miercoles son "
+                + numeroReparacionesDelDia);
+
+        //Formula de calculo del dia a dia 
+        reparacionesMiercoles = (float) (REPARACIONES *
+                numeroReparacionesDelDia);
+
+        //Resultado de las reparaciones del Lunes
+        //Que salga 3 deciamales
+        System.out.printf(" %.3f " + "€", reparacionesMiercoles);
+
+        //Jueves
+        //Entrada de datos
+        System.out.println(" Cuantas reparaciones a habido el Jueves?");
+
+        //Poner el escaler que sea un Int
+        numeroReparacionesDelDia = teclado.nextInt();
+        //Salida de datos de system out
+        System.out.println(" El numero de reparaciones de este Jueves son " +
+                numeroReparacionesDelDia);
+
+        //Formula de calculo del dia a dia 
+        reparacionesJueves = (float) (REPARACIONES * numeroReparacionesDelDia);
+
+        //Resultado de las reparaciones del Lunes
+        System.out.printf(" %.3f " + "€", reparacionesJueves);
+
+        //Viernes
+        //Entrada de datos
+        System.out.println(" Cuantas reparaciones a habido el Viernes?");
+
+        //Poner el escaler que sea un Int
+        numeroReparacionesDelDia = teclado.nextInt();
+        //Salida de datos de system out
+        System.out.println(" El numero de reparaciones de este Viernes son " +
+                numeroReparacionesDelDia);
+
+        //Formula de calculo del dia a dia 
+        reparacionesViernes = (float) (REPARACIONES * numeroReparacionesDelDia);
+
+        //Resultado de las reparaciones del Lunes
+        System.out.printf(" %.3f " + "€", reparacionesViernes);
+
+        //RecuentoTotal de la semana
+        //Formula para calcular toda la semana
+        reparacionesTotalSemana
+                = reparacionesLunes
+                + reparacionesMartes
+                + reparacionesMiercoles
+                + reparacionesJueves
+                + reparacionesViernes;
+        //Salida de datos de la semana
+        
+        System.out.printf(" Esta semana hemos ganado " + " %.3f" + "€", 
+                reparacionesTotalSemana);
+    }
+
+}
---
+++ src/main/java/Pablo/ParteB.java
@@ -0,0 +1,212 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pablo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author pablo
+ */
+public class ParteB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        /*
+        Modifica el ejercicio anterior para que
+        la lectura de datos y la salida por pantalla
+        usen diálogos JOptionPane.
+         Además, en esta parte, se debe preguntar cuánto se 
+        va a cobrar por reparación cada día de la semana (double) y 
+        luego preguntar cuántas reparaciones se han hecho cada día 
+        (int).
+        Una vez leídos los datos, calcula lo que se cobra cada día y al final 
+        de la semana, guardando los datos en tipos float. Usa un text 
+        block, limitando la
+        salida a 1 decimal, para usarlo en un JOptionPane 
+        que muestre la información de salida.
+        Guarda el resultado de evaluar si se ha cobrado al 
+        final de la semana más de 1000€, 
+        mostrando el resultado por consola.
+         */
+        //variables
+        double reparacionCosteRotatorio;
+        int reparacionesDelDia;
+        float gananciasLunes;
+        float gananciasMartes;
+        float gananciasMiercoles;
+        float gananciasJueves;
+        float gananciasViernes;
+        float reparacionesTotalSemana;
+
+        //Lunes
+        //Creacion de panel de datos para introducir datos
+        String costeLunesString = JOptionPane.showInputDialog(" Cuanto va a "
+                + " valer la reparacion el Lunes ");
+
+        //Transformacion de un dato(double) a un string
+        reparacionCosteRotatorio = Double.parseDouble(costeLunesString);
+
+        //Panel que muestra cuanto vale la reparacione
+        JOptionPane.showMessageDialog(null,"La reparacion vale hoy"
+                + reparacionCosteRotatorio + "€");
+
+        //Veces que se a reparado hoy lunes
+        String reparacionLunesString = JOptionPane.showInputDialog(" Cuantos "
+                + "cliente hechos hemos hoy");
+
+        //Transformacion de un dato(int) a un string
+        reparacionesDelDia = Integer.parseInt(reparacionLunesString);
+
+        //Salida de datos
+        //Formula
+        gananciasLunes = (float) (reparacionCosteRotatorio * 
+                reparacionesDelDia);
+        JOptionPane.showMessageDialog(null, "Hemos hecho "
+                + reparacionesDelDia + " Cliente y hemos ganado  "
+                + gananciasLunes + "€");
+
+        //Martes
+        //Creacion de panel de datos para introducir datos
+        String costeMartesString = JOptionPane.showInputDialog(" Cuanto va a"
+                + " valer la reparacion el Martes ");
+
+        //Transformacion de un dato(double) a un string
+        reparacionCosteRotatorio = Double.parseDouble(costeMartesString);
+
+        //Panel que muestra cuanto vale la reparacione
+        JOptionPane.showMessageDialog(null,"La reparacion vale hoy"
+                + reparacionCosteRotatorio + "€");
+
+        //Veces que se a reparado hoy
+        String reparacionMartesString = JOptionPane.showInputDialog(" Cuantos "
+                + "cliente hechos hemos hoy");
+
+        //Transformacion de un dato(int) a un string
+        reparacionesDelDia = Integer.parseInt(reparacionMartesString);
+
+        //Salida de datos
+        //Formula
+        gananciasMartes = (float) (reparacionCosteRotatorio * 
+                reparacionesDelDia);
+        JOptionPane.showMessageDialog(null, "Hemos hecho "
+                + reparacionesDelDia + " Cliente y hemos ganado  "
+                + gananciasMartes + "€");
+
+        //Miercoles 
+        //Creacion de panel de datos para introducir datos
+        String costeMiercolesString = JOptionPane.showInputDialog("Cuanto va a"
+                + " valer la reparacion el Miercoles ");
+
+        //Transformacion de un dato(double) a un string
+        reparacionCosteRotatorio = Double.parseDouble(costeMiercolesString);
+
+        //Panel que muestra cuanto vale la reparacione
+        JOptionPane.showMessageDialog(null,"La reparacion vale hoy"
+                + reparacionCosteRotatorio + "€");
+
+        //Veces que se a reparado hoy 
+        String reparacionMiercolesString = JOptionPane.showInputDialog("Cuantos"
+                + " cliente hechos hemos hoy");
+
+        //Transformacion de un dato(int) a un string
+        reparacionesDelDia = Integer.parseInt(reparacionMiercolesString);
+
+        //Salida de datos
+        //Formula
+        gananciasMiercoles = (float) (reparacionCosteRotatorio * 
+                reparacionesDelDia);
+        JOptionPane.showMessageDialog(null, "Hemos hecho "
+                + reparacionesDelDia + " Cliente y hemos ganado  "
+                + gananciasMiercoles + "€");
+
+        //Jueves 
+        //Creacion de panel de datos para introducir datos
+        String costeJuevesString = JOptionPane.showInputDialog(" Cuanto va a "
+                + "  valer la reparacion el Jueves ");
+
+        //Transformacion de un dato(double) a un string
+        reparacionCosteRotatorio = Double.parseDouble(costeJuevesString);
+
+        //Panel que muestra cuanto vale la reparacione
+        JOptionPane.showMessageDialog(null,"La reparacion vale hoy"
+                + reparacionCosteRotatorio + "€");
+
+        //Veces que se a reparado hoy 
+        String reparacionJuevesString = JOptionPane.showInputDialog(" Cuantos"
+                + " cliente hechos hemos hoy");
+
+        //Transformacion de un dato(int) a un string
+        reparacionesDelDia = Integer.parseInt(reparacionJuevesString);
+
+        //Salida de datos
+        //formula
+        gananciasJueves = (float) (reparacionCosteRotatorio * 
+                reparacionesDelDia);
+        JOptionPane.showMessageDialog(null, "Hemos hecho "
+                + reparacionesDelDia + " Cliente y hemos ganado  "
+                + gananciasJueves + "€");
+
+        //Viernes
+        //Creacion de panel de datos para introducir datos
+        String costeViernesString = JOptionPane.showInputDialog(" Cuanto va a "
+                + " valer la reparacion el Vienes. ");
+
+        //Transformacion de un dato(double) a un string
+        reparacionCosteRotatorio = Double.parseDouble(costeViernesString);
+
+        //Panel que muestra cuanto vale la reparaciones
+        JOptionPane.showMessageDialog(null,"La reparacion vale hoy"
+                + reparacionCosteRotatorio + "€");
+
+        //Veces que se a reparado hoy 
+        String reparacionViernesString = JOptionPane.showInputDialog(" Cuantos "
+                + "cliente "
+                + "hechos hemos hoy");
+
+        //Transformacion de un dato(int) a un string
+        reparacionesDelDia = Integer.parseInt(reparacionViernesString);
+
+        //Salida de datos
+        //Formula
+        gananciasViernes = (float) (reparacionCosteRotatorio *
+                reparacionesDelDia);
+        JOptionPane.showMessageDialog(null, "Hemos hecho "
+                + reparacionesDelDia + " Cliente y hemos ganado  "
+                + gananciasViernes + "€");
+
+        //Total de la semana
+        //Formula calculo
+        reparacionesTotalSemana
+                = gananciasLunes
+                + gananciasMartes
+                + gananciasMiercoles
+                + gananciasJueves
+                + gananciasViernes;
+
+        String resultadoSemana = """
+                                En Pcroto,
+                                Esta semana hemos ganado %.1f
+                               
+                                
+                                .
+                                """.formatted(reparacionesTotalSemana);
+
+        JOptionPane.showMessageDialog(null, resultadoSemana + "€");
+
+        //Declaracion Variable para saber si se
+        //superan los 1000 euros semanables como objetivo
+        boolean objetivo = reparacionesTotalSemana > 1000;
+        //Salida de datos en el panel
+        JOptionPane.showMessageDialog(null, "¿Se ha "
+                + "superando el objetivo de esta semana de 1000€? " + objetivo);
+
+    }
+
+}
